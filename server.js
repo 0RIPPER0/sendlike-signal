0ed436035a4dde71351f6708d6d6fdcba9d28454
0RIPPER0 <163846951+0RIPPER0@users.noreply.github.com>
2025-08-24 22:51:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,19 +15,29 @@ app.use(express.static(__dirname + "/public"));
 // --- socket.io signaling ---
 let roster = {}; // socket.id -> {id, name}
 
+// helper to broadcast full roster
+function sendRoster() {
+  io.emit("roster", Object.values(roster));
+}
+
 io.on("connection", (socket) => {
   console.log("Peer connected:", socket.id);
+
+  // give placeholder until announce
+  roster[socket.id] = { id: socket.id, name: "Anon" };
+  sendRoster();
 
   // client announces itself with a random name
   socket.on("announce", (data) => {
     roster[socket.id] = { id: socket.id, name: data.name || "Peer" };
-    io.emit("roster", Object.values(roster));
+    sendRoster();
   });
 
   // forward SDP offers/answers
   socket.on("offer", ({ to, sdp }) => {
     io.to(to).emit("offer", { from: socket.id, sdp });
   });
+
   socket.on("answer", ({ to, sdp }) => {
     io.to(to).emit("answer", { from: socket.id, sdp });
   });
@@ -40,7 +50,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("Peer disconnected:", socket.id);
     delete roster[socket.id];
-    io.emit("roster", Object.values(roster));
+    sendRoster();
   });
 });
 
